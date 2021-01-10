63f4f853e76999f55ee4fba0c367a7346564c456
huguesfils <huguesfils@icloud.com>
2021-01-10 20:28:50 +0100

    firebase connected
    Firebase connected, add a task to the database is ok.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   currentDate: string;
+  myTask: string;
+  addTask: boolean;
 
-  constructor() {
+  constructor( public afDB: AngularFireDatabase) {
     const date = new Date();
     const options = { weekday: 'long', month: 'long', day: 'numeric' };
     this.currentDate = date.toLocaleDateString('fr-FR', options);
   }
 
+  addTaskToFirebase() {
+    this.afDB.list('Tasks/').push({
+      id: +1,
+      text: this.myTask,
+      date: new Date().toISOString(),
+      checked: false
+    });
+    this.showForm();
+  }
+  
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
 }
