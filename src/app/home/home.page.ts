8388c29d7440913db7280d0fd69fcd89fe3fa135
huguesfils <huguesfils@icloud.com>
2021-01-11 12:44:02 +0100

    description added
    description slot works

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,18 +9,21 @@ import { AngularFireDatabase } from '@angular/fire/database';
 export class HomePage {
   currentDate: string;
   myTask: string;
+  description: string;
   addTask: boolean;
+  tasks = [];
 
   constructor( public afDB: AngularFireDatabase) {
     const date = new Date();
     const options = { weekday: 'long', month: 'long', day: 'numeric' };
     this.currentDate = date.toLocaleDateString('fr-FR', options);
+    this.getTasks();
   }
 
   addTaskToFirebase() {
     this.afDB.list('Tasks/').push({
-      id: +1,
       text: this.myTask,
+      description: this.description,
       date: new Date().toISOString(),
       checked: false
     });
@@ -30,5 +33,22 @@ export class HomePage {
   showForm() {
     this.addTask = !this.addTask;
     this.myTask = '';
+    this.description = '';
   }
+
+  getTasks() {
+    this.afDB.list('Tasks/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      this.tasks = [];
+      actions.forEach(action => {
+        this.tasks.push({
+          key: action.key,
+          text: action.payload.exportVal().text,
+          description: action.payload.exportVal().description,
+          date: action.payload.exportVal().date.substring(11, 16),
+          checked: action.payload.exportVal().checked
+        });
+      });
+    });
+  }
+
 }
